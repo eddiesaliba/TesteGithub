e4b29814d793292d4afaa24f690683cf2b95ac8e
Edwar Saliba Júnior <eddiesaliba@yahoo.com>
2022-04-01 21:43:13 -0300

    primeiro commit

---
+++ src/principal/Calculadora.java
@@ -0,0 +1,57 @@
+package principal;
+
+public class Calculadora {
+	private float val1;
+	private float val2;
+	float total;
+	
+	public Calculadora() {
+		val1 = 0;
+		val2 = 0;
+		total = 0;
+	}
+	
+	public Calculadora(float val1, float vv2) {
+		this.val1 = val1;
+		val2 = vv2;
+	}
+	
+	public void soma(float a, float b) {
+		total = a + b;
+	}
+	
+	void subtracao() {
+		total = val1 - val2;
+	}
+	
+	public float multiplicacao(float a) {
+		return (a * val2);
+	}
+	
+	public float divisao() {
+		if(val2 != 0)
+			return val1 / val2;
+		else
+			return 0;
+	}
+	
+	public void setVal1(float a) {
+		val1 = a;
+	}
+	
+	public float getVal1() {
+		return val1;
+	}
+
+	public float getVal2() {
+		return val2;
+	}
+
+	public void setVal2(float val2) {
+		this.val2 = val2;
+	}
+
+	public float getTotal() {
+		return total;
+	}
+}
---
+++ src/principal/Principal.java
@@ -0,0 +1,90 @@
+package principal;
+
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		float v1 = 0, v2 = 0, res = 0;
+		int resp, op;
+		Scanner sc = new Scanner(System.in);
+
+		Calculadora calc;
+		calc = new Calculadora();
+	
+		do {
+			System.out.println("--==[ Calculadora ]==--");
+			System.out.println("1 - soma");
+			System.out.println("2 - subtração");
+			System.out.println("3 - multiplicação");
+			System.out.println("4 - divisão");
+			System.out.println("Opção: ");
+			op = sc.nextInt();
+			
+			switch(op)
+			{
+				case 1:
+					System.out.println("--[ Adição ]--");
+					System.out.println("Digite o 1º valor");
+					v1 = sc.nextFloat();
+					System.out.println("Digite o 2º valor");
+					v2 = sc.nextFloat();
+					
+					calc.soma(v1, v2);
+					
+					res = calc.getTotal();
+					
+					System.out.println("Resultado: " + res);		
+					break;
+				case 2:
+					System.out.println("--[ Subtração ]--");
+					System.out.println("Digite o 1º valor");
+					v1 = sc.nextFloat();
+					System.out.println("Digite o 2º valor");
+					v2 = sc.nextFloat();
+					
+					calc.setVal1(v1);
+					calc.setVal2(v2);
+					
+					calc.subtracao();
+					
+//					res = calc.getTotal();
+					res = calc.total;
+					
+					
+					System.out.println("Resultado: " + res);		
+					break;
+				case 3:
+					System.out.println("--[ Multiplicação ]--");
+					System.out.println("Digite o 1º valor");
+					v1 = sc.nextFloat();
+					System.out.println("Digite o 2º valor");
+					v2 = sc.nextFloat();
+					
+					calc.setVal2(v2);
+					res = calc.multiplicacao(v1);
+					
+					System.out.println("Resultado: " + res);		
+					break;
+				case 4:
+					System.out.println("--[ Divisão ]--");
+					System.out.println("Digite o 1º valor");
+					v1 = sc.nextFloat();
+					System.out.println("Digite o 2º valor");
+					v2 = sc.nextFloat();
+					
+					calc.setVal1(v1);
+					calc.setVal2(v2);
+					res = calc.divisao();
+					
+					System.out.println("Resultado: " + res);		
+			}
+			
+			System.out.println("Deseja fazer outro cálculo? (1-sim/2-não)");
+			resp = sc.nextInt();
+			
+		}while(resp == 1);
+		
+		sc.close();
+	}
+}
